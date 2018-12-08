4153a3566ca2723acb0d97289ea4fa863b3f360b
eyalbd2 <38206414+eyalbd2@users.noreply.github.com>
2018-12-08 10:59:13 +0200

    Add files via upload

--- Acrobot/DQN_test.py
+++ Acrobot/DQN_test.py
@@ -1,16 +1,18 @@
-
 import torch
 import gym
+from utils import get_screen
 
 # Import environment
 env = gym.make('Acrobot-v1')
 # Define model and load pre-trained weights
-trained_Q_network = torch.load('trained_DQN_model')
+trained_Q_network = torch.load('trained_DQN_model', map_location=lambda storage, loc: storage)
 
 # Reset env
-state = env.reset()
+_ = env.reset()
+current_screen = get_screen(env)
+state = current_screen
 # Present trained behaviour over episodes
-num_test_episodes = 10
+num_test_episodes = 30
 
 episodes_passed = 0
 acc_episodic_reward = 0.0
@@ -19,7 +21,10 @@ while episodes_passed < num_test_episodes:
     # Choose action greedily
     action = trained_Q_network.select_action(state)
     # Act on env
-    state, reward, done, _ = env.step(action)
+    _, reward, done, _ = env.step(action)
+    last_screen = current_screen
+    current_screen = get_screen(env)
+    next_state = current_screen - last_screen
     # Add to accumulative reward
     acc_episodic_reward += reward
     # When episode is done - reset and print
@@ -31,4 +36,8 @@ while episodes_passed < num_test_episodes:
         episodes_passed += 1
         acc_episodic_reward = 0.0
 
-        state = env.reset()
+        _ = env.reset()
+        current_screen = get_screen(env)
+        next_state = current_screen
+
+    state = next_state
